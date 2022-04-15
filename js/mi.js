843958137d35ce940a8b74955a5ad7c96780ab99
MSIFE <mati598.sife@gmail.com>
2022-04-15 15:45:53 -0300

    FALLA NO SE POR QUE

---
+++ js/mi.js
@@ -0,0 +1,40 @@
+let aEdades = [];
+let aTrabajos = [];
+let aNombres = [];
+let aLesiones = [];
+let aPeso = [];
+
+if(localStorage.getItem("edades_") != null){
+    aEdades = JSON.parse(localStorage.getItem('edades_'))
+    aTrabajos = JSON.parse(localStorage.getItem('trabajos_'));
+    aNombres = JSON.parse(localStorage.getItem('nombres_'));
+    aLesiones = JSON.parse(localStorage.getItem('lesiones_'));
+    aPeso = JSON.parse(localStorage.getItem('peso_'));
+}
+
+let elementoBotonRegistrar = document.querySelector('#btnRegistrar')
+
+elementoBotonRegistrar.addEventListener("click", registroAlumno);
+
+function registroAlumno(){
+    let nEdad = document.querySelector("#txtEdad").value;
+    let sTrabajo = document.querySelector("#txtTrabajo").value;
+    let sNombre = document.querySelector("#txtNombre").value;
+    let sLesion = document.querySelector("#txtLesiones").value;
+    let sPeso= document.querySelector("#txtPeso").value;
+
+    aEdades.push(nEdad);
+    aTrabajos.push(sTrabajo);
+    aNombres.push(sNombre);
+    aLesiones.push(sLesion);
+    aPeso.push(sPeso);
+
+    localStorage.setItem('edades_', JSON.stringify(aEdades));
+    localStorage.setItem('trabajos_', JSON.stringify(aTrabajos));
+    localStorage.setItem('nombres_', JSON.stringify(aNombres));
+    localStorage.setItem('lesion_', JSON.stringify(aLesiones));
+    localStorage.setItem('peso_', JSON.stringify(aPeso));
+    
+    llenarTabla();
+}
+
---
+++ js/mostrar.js
@@ -0,0 +1,49 @@
+llenarTabla();
+
+function llenarTabla(){
+    let tbody = document.querySelector("#tblAlumno tbody");
+
+    tbody.innerHTML = "";
+
+    let aEdades = JSON.parse(localStorage.getItem('edades_'));
+    let aTrabajos = JSON.parse(localStorage.getItem('trabajos_'));
+    let aNombres = JSON.parse(localStorage.getItem('nombres_'));
+    let aLesiones = JSON.parse(localStorage.getItem('lesiones_'));
+    let aPeso = JSON.parse(localStorage.getItem('peso_'));
+
+let nCantidadAlumnos = aEdades.length;
+
+for(let i = 0;i < nCantidadAlumnos; i++){
+    let fila = document.createElement('tr');
+
+    let celdaEdad = document.createElement('td');
+    let celdaTrabajo = document.createElement('td');
+    let celdaNombre = document.createElement('td');
+    let celdaLesion = document.createElement('td');
+    let celdaPeso = document.createElement('td');
+
+    let nodoTextoEdad = document.createTextNode(aEdades[i]);
+    let nodoTextoTrabajo = document.createTextNode(aTrabajos[i]);
+    let nodoTextoNombre = document.createTextNode(aNombres[i]);
+    let nodoTextoLesion = document.createTextNode(aLesiones[i]);
+    let nodoTextoPeso = document.createTextNode(aPeso[i]);
+
+        
+    celdaEdad.appendChild(nodoTextoEdad);
+    celdaTrabajo.appendChild(nodoTextoTrabajo);
+    celdaNombre.appendChild(nodoTextoNombre);
+    celdaLesion.appendChild(nodoTextoLesion);
+    celdaPeso.appendChild(nodoTextoPeso);
+
+    fila.appendChild(celdaEdad);
+    fila.appendChild(celdaTrabajo);
+    fila.appendChild(celdaNombre);
+    fila.appendChild(celdaLesion);
+    fila.appendChild(celdaPeso);
+    
+
+    tbody.appendChild(fila);
+}
+
+}
+
